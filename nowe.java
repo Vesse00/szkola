4b93fd19e2419b18fe692f8b3678d3a3121bcc87
Vesse00 <vessepl@gmail.com>
2021-02-08 08:36:20 +0100

    Update nowe.java

--- nowe.java
+++ nowe.java
@@ -3,9 +3,10 @@ import java.io.*;
 
 class nowe {
     public static void main(String[] args) {
-        File kwadrat = new File("kawdrat.txt");
-        File prostokat = new File("prostokat.txt");
         Scanner sc = new Scanner(System.in);
+        /*File kwadrat = new File("kawdrat.txt");
+        File prostokat = new File("prostokat.txt");
+        
 
         System.out.println("Wybierz fgure(kwadrat lub prostokat): ");
         String figura = sc.next();
@@ -27,6 +28,16 @@ class nowe {
                 } catch (IOException e) {
                     System.out.println("BLAD: "+e.toString());
                 }
+            }*/
+            System.out.println("Jaka figure chcesz wybrac(1-kwadrat 2-prostokat): ");
+            String figura = sc.next();
+
+            switch(figura){
+                case "prostokat":
+                    System.out.println("Podaj dlugosc bokow (a;b): ");
+                    String prost = sc.next();
+                    String rozmiar[] = prost.split(";");
+                    //System.out.println(rozmiar[0]+" "+rozmiar[1]);
             }
 
             
