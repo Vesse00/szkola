d7ce70cb8dd22452bed02cac5f323c569b04446e
Vesse00 <vessepl@gmail.com>
2021-02-09 09:27:25 +0100

    Update nowe.java

--- nowe.java
+++ nowe.java
@@ -4,47 +4,33 @@ import java.io.*;
 class nowe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        /*File kwadrat = new File("kawdrat.txt");
-        File prostokat = new File("prostokat.txt");
-        
-
-        System.out.println("Wybierz fgure(kwadrat lub prostokat): ");
-        String figura = sc.next();
-
-        if (figura == "kwadrat") {
-            try {
-                FileWriter fw = new FileWriter(kwadrat);
-                fw.write(figura);
-                fw.close();
-            } catch (IOException e) {
-                System.out.println("BLAD: "+e.toString());
-            }
-
-            }else if(figura == "prostokat"){
-                try {
-                    FileWriter fw = new FileWriter(prostokat);
-                    fw.write(figura);
-                    fw.close();
-                } catch (IOException e) {
-                    System.out.println("BLAD: "+e.toString());
-                }
-            }*/
-            System.out.println("Jaka figure chcesz wybrac(1-kwadrat 2-prostokat): ");
+       
+            System.out.println("Jaka figure chcesz wybrac(kwadrat prostokat): ");
             String figura = sc.next();
 
             switch(figura){
                 case "prostokat":
-                    System.out.println("Podaj dlugosc boku a: ");
+                    System.out.print("Podaj dlugosc boku a: ");
                     int a = sc.nextInt();
 
-                    System.out.println("Podaj dlugosc boku b: ");
+                    System.out.print("Podaj dlugosc boku b: ");
                     int b = sc.nextInt();
                     
-                    //System.out.println(a+";"+b);
+                    System.out.println("("+a+";"+b+")");
 
 
                     if(a == b){
-                        System.out.println(a+" jest takie samo jak "+b+" to nie jest prostokat");
+                        System.out.println(a+"(a) jest takie samo jak "+b+"(b) to nie jest prostokat");
+                    }else if(a == 0 || b == 0 || a<0 || b<0){
+                        System.out.println("Nie mozna podawac wartosci rownych oraz mniejszych niz 0");
+                        if(a<=0){
+                            System.out.println("Bok a = "+a);
+                        }else if(b<=0){
+                            System.out.println("Bok b = "+b);
+                        }else{
+                            break;
+                        }
+                        
                     }else{
                         for(var i = 0;i < a;i++){
                             for(var j = 0;j < b;j++){
@@ -52,11 +38,45 @@ class nowe {
                             }
                             System.out.print("\n");
                         }
+
+                        double poleprost = a *b;
+                        int obwodprost = (2*a)+(2*b);
+                        System.out.println("Pole prostokata: "+poleprost);
+                        System.out.println("Obwod prostokata: "+obwodprost);
                     }
 
 
-                    
+                    break;
 
+                case "kwadrat":
+                    System.out.print("Podaj dlugosc kwadratu: ");
+                    int kw = sc.nextInt();
+
+                    System.out.println("("+kw+";"+kw+")");
+
+                    if(kw <=0){
+                        System.out.println("Dlugosc kwadratu nie moze byc rowne ani mniejsze niz 0 ");
+
+                    }else{
+                        for(var i = 0;i < kw;i++){
+                            for(var j = 0;j < kw;j++){
+                                System.out.print("*");
+                            }
+                            System.out.print("\n");
+                        }
+
+                        double polekw = kw*kw;
+                        int obwkw = (4*kw);
+                        System.out.println("Pole kwadratu: "+polekw);
+                        System.out.println("Obwod kwadratu: "+obwkw);
+                    }
+                    
+                    break;
+                case "exit":
+                    System.exit(0);
+                    break;
+
+                
             }
 
             
